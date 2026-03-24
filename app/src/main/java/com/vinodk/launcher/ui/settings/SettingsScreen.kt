7bd20a09bf740124c6734ee1e82a0a54a7be7061
MKVK8277 <sbpi1970@gmail.com>
2026-03-24 14:54:27 +0530

    Import formatMinutes function in SettingsScreen.kt

--- app/src/main/java/com/vinodk/launcher/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/vinodk/launcher/ui/settings/SettingsScreen.kt
@@ -1,5 +1,5 @@
 package com.vinodk.launcher.ui.settings
-
+import com.vinodk.launcher.ui.timers.formatMinutes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
